7a6ef9a6bb3f2462d649dc8dc04d0c960508cf4e
LuisMartinezMoreno <luismartinezmoreno.it@gmail.com>
2023-04-18 16:26:03 -0400

    Bug fixing

--- dataBaseUtil.py
+++ dataBaseUtil.py
@@ -1,24 +1,22 @@
 import pymongo
 from pymongo.server_api import ServerApi
 
-def connect():
-    # Replace the following with your MongoDB Atlas cluster connection string
-    # Don't forget to replace <username>, <password>, and <dbname> with your own values
-    connection_string = "mongodb+srv://luis:QvcHEiYx59Svisag@passmanager.sixpzvi.mongodb.net/?retryWrites=true&w=majority"
 
-    # Create a MongoClient object
-    client = pymongo.MongoClient(connection_string, server_api=ServerApi('1'))
+connection_string = "mongodb+srv://luis:QvcHEiYx59Svisag@passmanager.sixpzvi.mongodb.net/?retryWrites=true&w=majority"
 
-    # Access the database
-    db = client.PassManager
+# Create a MongoClient object
+client = pymongo.MongoClient(connection_string, server_api=ServerApi('1'))
 
-    # Access a collection within the database
-    collection = db.<collection-name>
+# Access the database
+db = client.PassManager
+
+# Access a collection within the database
+collection = db.Accounts
 
 
 def find():
     # Access the collection you want to search
-    collection = db.<collection-name>
+    collection = db.Accounts
 
     # Search for all documents in the collection
     results = collection.find()
@@ -27,13 +25,26 @@ def find():
     for result in results:
         print(result)
 
-def insert():
+def searchByUserID(user_id):
+    # Access the collection you want to search
+    collection = db.Accounts
+
+    # Search for documents that match the userID
+    results = collection.find({"userID": user_id})
+
+    # Iterate over the results and print each document
+    for result in results:
+        print(result)
+
+def insert(accountPK, userID,EncryptedKey, HashedKey, IV):
     # Create a document to insert
-    document = {"name": "Jane Doe", "age": 30}
+    account = {"accountPK":accountPK, "userID":userID, "EncryptedKey":EncryptedKey, "HashedKey":HashedKey, "IV": IV}
 
     # Insert the document into the collection
-    result = collection.insert_one(document)
+    result = collection.insert_one(account)
 
     # Print the ID of the inserted document
     print(result.inserted_id)
 
+if __name__ == '__main__':
+    insert("accountPK", "userID","EncryptedKey", "HashedKey", "IV")
