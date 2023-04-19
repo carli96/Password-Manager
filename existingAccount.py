e81305946f2a9e3587443beeca41373bdfb8cd6e
carli96 <cfial007@fiu.edu>
2023-04-18 20:05:25 -0400

    account information

--- existingAccount.py
+++ existingAccount.py
@@ -9,13 +9,35 @@ import maskpass
 
 def window(user, passw, web):
     sg.theme("DarkBlue")
-    layout = [[sg.Text('WebPage:')],
-              [sg.Text(web)],
-              [sg.Text('User:')],
-              [sg.Text(user)],
-              [sg.Text('Password:')],
-              [sg.Text(passw)]]
-    window = sg.Window('Window Title', layout)
+    column_to_centered = [[sg.Text('WebPage:', font=('Arial Bold', 20),
+                                   size=20, expand_x=True,
+                                   justification='center')],
+                          [sg.Text(web, font=('Arial Bold', 14),
+                                   size=20, expand_x=True,
+                                   justification='center')],
+                          [sg.Text('User:', font=('Arial Bold', 20),
+                                   size=20, expand_x=True,
+                                   justification='center')],
+                          [sg.Text(user, font=('Arial Bold', 14),
+                                   size=20, expand_x=True,
+                                   justification='center')],
+                          [sg.Text('Password:', font=('Arial Bold', 20),
+                                   size=20, expand_x=True,
+                                   justification='center')],
+                          [sg.Text(passw, font=('Arial Bold', 14),
+                                   size=20, expand_x=True,
+                                   justification='center')],
+                          [sg.Text(" ", expand_x=True,
+                                   justification='center')],
+                          [sg.Text(" ", expand_x=True,
+                                   justification='center')],
+                          [sg.Button("Cancel", auto_size_button=20)]]
+    layout = [[sg.VPush()],
+              [sg.Push(), sg.Column(column_to_centered,
+                                    element_justification='c'), sg.Push()],
+              [sg.VPush()]]
+    window = sg.Window("Account Information", layout,
+                       size=(500, 300))
 
     while True:
         event, values = window.read()
